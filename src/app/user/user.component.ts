41bb24f47584070ed56024cf8d2ace9897ca4a03
Filip Pollum <filip.pollum@gmail.com>
2024-06-18 00:48:20 +0200

    addition of users&dynamic and their dynamic rotation

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import { DUMMY_USERS } from '../dummy-users';
+
+const randomUser = Math.floor(Math.random() * DUMMY_USERS.length)
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -8,5 +11,14 @@ import { Component } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
+  selectedUser = DUMMY_USERS[randomUser];
 
+  get imagePath(){
+    return 'assets/users/' + this.selectedUser.avatar
+  }
+
+  onSelectedUser(){
+    const randomUser = Math.floor(Math.random() * DUMMY_USERS.length)
+    this.selectedUser = DUMMY_USERS[randomUser];
+  }
 }
